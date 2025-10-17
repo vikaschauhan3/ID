9edb0d5465e4ff65049b6e4bb8e895b85258f6cf
vikaschauhan3 <vikas.chauhan3@gmail.com>
2025-10-17 15:36:06 +0400

    Update portfolio-manifest.js

--- assets/js/portfolio-manifest.js
+++ assets/js/portfolio-manifest.js
@@ -1,19 +1,5 @@
----
----
-
-/* Portfolio manifest: controls order + titles for prev/next */
+// assets/js/portfolio-manifest.js
 window.PortfolioManifest = [
-  {
-    title: "Client Centered Communication",
-    slug: "client_centered_communication",
-    href: "{{ '/client_centered_communication.html' | relative_url }}"
-  },
-  {
-    title: "CMI Video Series",
-    slug: "cmi_video_series",
-    href: "{{ '/cmi_video_series.html' | relative_url }}"
-  }
-
-  // Add more in the order you want them to appear:
-  // { title: "Project Name", slug: "project_slug", href: "{{ '/project_slug.html' | relative_url }}" },
+  { slug: "client_centered_communication", title: "Client Centered Communication", href: "{{ '/client_centered_communication.html' | relative_url }}" },
+  { slug: "cmi_video_series", title: "CMI Certification Video Guide Series", href: "{{ '/cmi_video_series.html' | relative_url }}" }
 ];
