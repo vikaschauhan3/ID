43d3f68b46378e4aa4ae6895a1ad0a50cb8200eb
vikaschauhan3 <vikas.chauhan3@gmail.com>
2025-10-23 13:10:13 +0400

    assets reordring

---
+++ portfolio-manifest.js
@@ -0,0 +1,23 @@
+---
+---
+
+/* Portfolio manifest: controls order + titles for prev/next */
+window.PortfolioManifest = [
+  {
+    title: "Client Centered Communication",
+    slug: "client_centered_communication",
+    href: "{{ '/client_centered_communication.html' | relative_url }}"
+  },
+  {
+    title: "CMI Video Series",
+    slug: "cmi_video_series",
+    href: "{{ '/cmi_video_series.html' | relative_url }}"
+  },
+  {
+    title: "Innovation Labs",
+    slug: "innovation_labs",
+    href: "{{ '/innovation_labs.html' | relative_url }}"
+  }
+  // Add more in the order you want them to appear:
+  // { title: "Project Name", slug: "project_slug", href: "{{ '/project_slug.html' | relative_url }}" },
+];
