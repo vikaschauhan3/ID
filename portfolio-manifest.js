fd37239850167ef56c48cd9c7cfd8b4363fa283e
vikaschauhan3 <vikas.chauhan3@gmail.com>
2025-10-23 13:44:28 +0400

    details page update

--- portfolio-manifest.js
+++ portfolio-manifest.js
@@ -6,17 +6,22 @@ window.PortfolioManifest = [
   {
     title: "Client Centered Communication",
     slug: "client_centered_communication",
-    href: "{{ '/client_centered_communication.html' | relative_url }}"
+    href: "{{ '/assets/portfolio/client_centered_communication/index.html' | relative_url }}"
   },
   {
     title: "CMI Video Series",
     slug: "cmi_video_series",
-    href: "{{ '/cmi_video_series.html' | relative_url }}"
+    href: "{{ '/assets/portfolio/cmi_video_series/index.html' | relative_url }}"
   },
   {
     title: "Innovation Labs",
     slug: "innovation_labs",
-    href: "{{ '/innovation_labs.html' | relative_url }}"
+    href: "{{ '/assets/portfolio/innovation_labs/index.html' | relative_url }}"
+  },
+  {
+    title: "Video Based Learning",
+    slug: "video_based_learning",
+    href: "{{ '/assets/portfolio/video_based_learning/index.html' | relative_url }}"
   }
   // Add more in the order you want them to appear:
   // { title: "Project Name", slug: "project_slug", href: "{{ '/project_slug.html' | relative_url }}" },
